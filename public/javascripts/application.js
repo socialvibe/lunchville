1a64c6e6770722ff66344cf15d519e231f553890
Ed Soed <edwin@svnetwork.com>
2011-04-01 13:04:03 -0700

    update

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,14 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(document).ready(function(){
+	var ph = pageHeight();
+	$('div#content').height( ph - $('div#header').height() - 20 );
+});
+ 
+function pageHeight()
+{
+	return  window.innerHeight != null? window.innerHeight : document.documentElement &&
+	document.documentElement.clientHeight ?  document.documentElement.clientHeight :
+	document.body != null? document.body.clientHeight : null;
+}
