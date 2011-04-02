fe3ba7462ae61a4311c75bb41eb910d9641ed1f7
jesse <jesse@svnetwork.com>
2011-04-01 18:45:11 -0700

    messing with overall css styling

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,11 +1,7 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-$(document).ready(function(){
-	var ph = pageHeight();
-	$('div#content').height( ph - $('div#header').height() - 40 );
-});
- 
+
 function pageHeight()
 {
 	return  window.innerHeight != null? window.innerHeight : document.documentElement &&
